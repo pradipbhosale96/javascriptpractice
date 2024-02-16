ec5865fecc1c5a46a7b508c9129369b4b2eabd52
Pradip <pradipbhosale1411@gmail.com>
2024-02-16 22:21:24 +0530

    commit Check

---
+++ Fetch-Products-Filter/index.js
@@ -0,0 +1,12 @@
+// const url="https://fakestoreapi.com/products"
+// const fetchProducts = async()=>{
+//     try{
+//     const res= await fetch(url);
+//     return await res.json();
+//     }catch(error){
+//         return error
+//     }
+// };
+
+// console.log("fetchProducts",fetchProducts());
+console.log("Hello World");
